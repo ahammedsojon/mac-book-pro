65ad6aed5552f5e45887f01bdf96fd4088b11927
ahammedsojon <ahammedsojon000@gmail.com>
2021-08-21 14:37:33 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function extraCost(variant, cost) {
     document.getElementById(variant).innerText = cost;
     // update price
     document.getElementById('total-price').innerText = updatePrice();
-    document.getElementById('center-price').innerText = updatePrice();
+    document.getElementById('price-center').innerText = updatePrice();
 }
 function updatePrice() {
     const memoryCostText = document.getElementById('memory-cost').innerText;
@@ -14,16 +14,28 @@ function updatePrice() {
     const totalPrice = 1299 + memoryCostPrice + storageCostPrice + deliveryCharge;
     return totalPrice;
 }
-
+function addPromoCode() {
+    const promoInput = document.getElementById('promo-input');
+    let totalPriceText = document.getElementById('price-center');
+    const totalPrice = parseInt(totalPriceText.innerText);
+    if (promoInput.value == 'stevekaku') {
+        const discount = (totalPrice * 20) / 100;
+        totalPriceText.innerText = totalPrice - discount;
+        promoInput.value = '';
+        document.getElementById('promo-btn').disabled = true;
+    }
+}
 // extra memory cost
 document.getElementById('memory').addEventListener('click', function (e) {
     const id = e.target.getAttribute('id');
     if (id == 'memory-8gb') {
         extraCost('memory-cost', '0');
+        document.getElementById('promo-btn').disabled = false;
     }
 
     if (id == 'memory-16gb') {
         extraCost('memory-cost', '180');
+        document.getElementById('promo-btn').disabled = false;
     }
 })
 
@@ -32,12 +44,15 @@ document.getElementById('storage').addEventListener('click', function (e) {
     const id = e.target.getAttribute('id');
     if (id == 'storage-256gb') {
         extraCost('storage-cost', '0');
+        document.getElementById('promo-btn').disabled = false;
     }
     if (id == 'storage-512gb') {
         extraCost('storage-cost', '100');
+        document.getElementById('promo-btn').disabled = false;
     }
     if (id == 'storage-1tb') {
         extraCost('storage-cost', '180');
+        document.getElementById('promo-btn').disabled = false;
     }
 })
 // delivery charge
@@ -45,8 +60,15 @@ document.getElementById('delivery').addEventListener('click', function (e) {
     const id = e.target.getAttribute('id');
     if (id == 'delivery-25-aug') {
         extraCost('delivery-charge', '0');
+        document.getElementById('promo-btn').disabled = false;
     }
     if (id == 'delivery-21-aug') {
         extraCost('delivery-charge', '20');
+        document.getElementById('promo-btn').disabled = false;
     }
+})
+
+// add promo code
+document.getElementById('promo-btn').addEventListener('click', function (e) {
+    addPromoCode();
 })
